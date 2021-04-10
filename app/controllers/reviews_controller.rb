6ea5f15369fcacc8ed6ee5cf508540c628cae8e7
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-10 21:57:48 +0200

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  def new
+    @girl = Girl.find(params[:girl_id])
+    @review = Review.new
+  end
+  def create
+    @girl = Girl.find(params[:girl_id])
+    @review = Review.new(review_params)
+    @review.girl = @girl
+    if @review.save
+      redirect_to girl_path(@girl)
+    else
+      render :new
+    end
+  end
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to girl_path(@review.girl)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :girl
+  validates :content, presence: true
 end
